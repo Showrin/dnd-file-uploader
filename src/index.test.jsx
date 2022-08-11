e42082327995ebc0955a3cb926a0a98edf39a6dc
Showrin <showrinece15@gmail.com>
2022-08-11 07:02:00 +0600

    :wrench: Setup jest and react-testing-library with babel

---
+++ src/index.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import App from "./index.jsx";
+
+describe("App tests", () => {
+  it("should contains the heading 1", () => {
+    render(<App />);
+    const heading = screen.getByText(/App/i);
+    expect(heading).toBeInTheDocument();
+  });
+});
