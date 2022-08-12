a714976d51871c2223691f0c315ce9d439d32e57
Showrin <showrinece15@gmail.com>
2022-08-12 07:11:43 +0600

    :alembic: Add unused string to trigger circleCI runs

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>App Content</div>;
+  return <div>App Content Text</div>;
 };
 
 export default App;
--- src/index.test.jsx
+++ src/index.test.jsx
@@ -5,7 +5,7 @@ import App from "./index.jsx";
 describe("App tests", () => {
   it("should contains the heading 1", () => {
     render(<App />);
-    const heading = screen.getByText(/App/i);
+    const heading = screen.getByText(/App Content Text/i);
     expect(heading).toBeInTheDocument();
   });
 });
